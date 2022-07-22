67bc5df0429c6a3d4e0bbeffa3d753c2c99ae5d1
JWadee <58128135+JWadee@users.noreply.github.com>
2022-07-22 13:16:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var path = require('path');
 const {MongoClient} = require("mongodb");
 const mailer = require('./mailer');
 
